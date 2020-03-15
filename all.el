3b1053be276148e77f345a12b782019d452fda85
conao3 <conao3@gmail.com>
2020-03-15 15:51:21 +0900

    distriute this package under GPLv3

--- all.el
+++ all.el
@@ -1,24 +1,24 @@
 ;;; all.el --- Edit all lines matching a given regexp
 
 ;; Copyright (C) 1985-1987,1992,1994,2011-2012 Free Software Foundation, Inc.
+;; Copyright (C) 2020  Naoya Yamashita
 
 ;; Author: Per Abrahamsen <abraham@dina.kvl.dk>
 ;; Version: 1.0
 ;; Keywords: matching
 
-;; This program is free software; you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-;;
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
